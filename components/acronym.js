eae4b9a915e57898c2a397f5b1f1df7d84760846
Jacob Ford <j@cobford.com>
2019-05-26 15:24:15 -0400

    Fix redundant nested h1.logotype

--- components/acronym.js
+++ components/acronym.js
@@ -1,6 +1,6 @@
 const acronyms = [
-  <h1 class="logotype"><span class="remain">I</span><span class="compress">ntergallactic </span><span class="remain">B</span><span class="compress">allistic </span><span class="remain">5</span><span class="compress">,000 </span><span class="expand">k</span></h1>,
-  <h1 class="logotype"><span class="remain">I</span><span class="compress">naugural </span><span class="remain">B</span><span class="compress">all $</span><span class="remain">5</span><span class="compress">,000 </span><span class="expand">k</span></h1>,
+  <><span class="remain">I</span><span class="compress">ntergallactic </span><span class="remain">B</span><span class="compress">allistic </span><span class="remain">5</span><span class="compress">,000 </span><span class="expand">k</span></>,
+  <><span class="remain">I</span><span class="compress">naugural </span><span class="remain">B</span><span class="compress">all $</span><span class="remain">5</span><span class="compress">,000 </span><span class="expand">k</span></>,
 ]
 
 const randIndex = Math.floor(Math.random() * acronyms.length)
